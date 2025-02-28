5e1546967b1d2be1c81df905886d80583fcd39f3
Asmit Pathi <asmitpathi27@gmail.com>
2025-02-28 11:44:31 +0530

    add Footer import in Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './Header/Header'
-import Footer from './Footer/footer'
+import Footer from './Footer/Footer'
 import { Outlet } from 'react-router-dom' //Outlet willuse the layout as a base and wherever we use Outlet we can change that part of the layout
 
 function Layout() {
